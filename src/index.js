98072e6cecd8419e9555dabc9782e0234d91bffb
Samuel Hinga <samuelhinga200@gmail.com>
2022-04-25 03:37:16 +0300

    Complete

--- src/index.js
+++ src/index.js
@@ -1,3 +1,24 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  let form = document.querySelector("form#create-task-form")
+  form.addEventListener("submit", (e) => {
+    e.preventDefault()
+    updateToDoItem(e.target.description.value)
+
+    form.reset()
+  })
 });
+function updateToDoItem(item) {
+  let listItem = document.createElement('li');
+  let deletebtn = document.createElement('button')
+  deletebtn.addEventListener('click', deleteToDo)
+
+  deletebtn.textContent = "x"
+  listItem.textContent = `${item} `
+  listItem.appendChild(deletebtn)
+  document.getElementById("tasks").append(listItem)
+}
+
+function deleteToDo(e) {
+  e.target.parentNode.remove()
+}
